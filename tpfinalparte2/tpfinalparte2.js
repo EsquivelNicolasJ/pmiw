d83d87ff23ad0301b97abc2910d3d74ccce8f931
EsquivelNicolasJ <nicolasjavieresquivel152@gmail.com>
2025-11-15 20:00:05 -0300

    Add files via upload

---
+++ tpfinalparte2/tpfinalparte2.js
@@ -0,0 +1,20 @@
+let pntlJuego;
+
+function setup() {
+  createCanvas(640, 480);
+  pntlJuego = new Juego();
+}
+
+function draw() {
+  image(fondo, 0, 0);
+  if (pntlJuego && typeof pntlJuego.dibujar === 'function') pntlJuego.dibujar();
+}
+
+function mousePressed() {
+  if (typeof getAudioContext === 'function' && getAudioContext().state !== 'running') {
+    getAudioContext().resume();
+  }
+  if (pntlJuego && typeof pntlJuego.onMousePressed === 'function') {
+    pntlJuego.onMousePressed(mouseX, mouseY);
+  }
+}
